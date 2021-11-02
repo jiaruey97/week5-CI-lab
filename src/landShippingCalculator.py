cff428daaca0d6fd55de70b1df201889fb935aad
jiaruey97 <jiaruey.tan.2019@sis.smu.edu.sg>
2021-11-02 18:40:08 +0800

    error change

--- src/landShippingCalculator.py
+++ src/landShippingCalculator.py
@@ -1,13 +1,15 @@
-from shippingCalculator import ShippingCalculator 
+from shippingCalculator import ShippingCalculator
 from countryCharges import CountryCharges
 
 class LandShippingCalculator (ShippingCalculator):
     def __init__(self, custNm, custContact):
-        ShippingCalculator.__init__(self,custNm, custContact)
+        ShippingCalculator.__init__(self, custNm, custContact)
+
     def computeCharges(self):
         return super().baseCharge() + self.landPackagingCharge() + super().customCharge() + self.landFreightCharges()
-    
+
     def landPackagingCharge(self):
         return (super().getSize()*super().getWeight())*0.9
+
     def landFreightCharges(self):
         return (super().getSize()*0.25) + (super().getWeight()*0.75) + 10
